c78036dc26add9bbda3a23a30d50031894a8a474
WillForan <willforan@gmail.com>
2017-05-04 13:18:10 -0400

    add Yeo17 and FSL209c

--- getage.bash
+++ getage.bash
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
-sed 's/_/ /' txt/subj_date.txt  | while read s d; do psql -h arnold.wpic.upmc.edu lncddb lncd -AF$'\t' -qtc  "select lunaid,ymd,age from visits_view where lunaid like '$s' and ymd like '$d'";  done | tee txt/subj_date_age.txt
+sed 's/_/ /' txt/subj_date.txt  | 
+while read s d; do
+  psql -h arnold.wpic.upmc.edu lncddb lncd -AF$'\t' -qtc  "select lunaid,ymd,age from visits_view where lunaid like '$s' and ymd like '$d'"
+
+  if [ "$d" == 20151118 -a "$s" == 11462 ]; then
+   # missing this subject b/c visit date is 2 days later
+   # age will be under by ~ 2/365
+   echo -e "11462\t20151116\t14.7104722792608"|sed 's/20151116/20151118/' 
+  fi
+done | tee txt/subj_date_age.txt
+
